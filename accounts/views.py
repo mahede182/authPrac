3939441657f664742837fb31bdb6c79dbc741895
mahede182 <mahede.object@gmail.com>
2024-12-30 00:57:47 +0600

    docs: without login you are not visit home

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+# A mixin for users who have logged in, which checks the login requirement
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
-class Home(TemplateView):
+class Home(LoginRequiredMixin, TemplateView):
+    login_url = 'signin'
     template_name = "accounts/home.html"
 
 
-class Signin(TemplateView):
-    template_name = "accounts/signIn.html"
-
+class Signin(View):
+    def get(self, *args, **kwargs):
+        return render(self.request, "accounts/signIn.html")
+    def post(self, request):
+        pass
 
 class Signup(TemplateView):
     template_name = "accounts/signup.html"
